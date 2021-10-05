f186d89299af1cb08579be726431d041cc73eb35
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2021-10-05 18:00:26 -0300

    Criação de controller

---
+++ src/main/java/com/temperosoft/webflux/controller/PlaylistController.java
@@ -0,0 +1,37 @@
+package com.temperosoft.webflux.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.temperosoft.webflux.document.Playlist;
+import com.temperosoft.webflux.services.PlaylistService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class PlaylistController {
+
+	@Autowired
+	PlaylistService service;
+	
+	@GetMapping(value="/playlist")
+	public Flux<Playlist> getPlaylist() {
+		return service.findAll();
+	}
+	
+	@GetMapping(value="/playlist/{id}")
+	public Mono<Playlist> getPlaylistId(@PathVariable String id) {
+		return service.findById(id);
+	}
+	
+	@PostMapping(value="/playlist")
+	public Mono<Playlist> save(@RequestBody Playlist playlist) {
+		return service.save(playlist);
+	}
+	
+}
